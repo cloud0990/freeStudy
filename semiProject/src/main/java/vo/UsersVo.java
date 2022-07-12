fcf7087c74ad17159d07a0d2a2ee6f3c70cf92c4
LAPTOP-EI7PO15S\cloud099 <kdjkuku@gmail.com>
2022-07-13 00:00:16 +0900

    2022_0712 : users 테이블 생성 및 Vo 생성

---
+++ semiProject/src/main/java/vo/UsersVo.java
@@ -0,0 +1,39 @@
+package vo;
+
+public class UsersVo {
+	
+	int    u_idx;
+	String u_id;
+	String u_pwd;
+	String u_nickname;
+	String zipcode;
+	String u_addr;
+	String u_grade;
+	String u_regdate;
+	
+	
+	public void setU_idx(int u_idx) {
+		this.u_idx = u_idx;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+	public void setU_pwd(String u_pwd) {
+		this.u_pwd = u_pwd;
+	}
+	public void setU_nickname(String u_nickname) {
+		this.u_nickname = u_nickname;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public void setU_addr(String u_addr) {
+		this.u_addr = u_addr;
+	}
+	public void setU_grade(String u_grade) {
+		this.u_grade = u_grade;
+	}
+	public void setU_regdate(String u_regdate) {
+		this.u_regdate = u_regdate;
+	}
+}
